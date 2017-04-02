f94ead518f8fb0ae41a50998cc8c656120f56d01
milestyle <milesenglish88@gmail.com>
2017-04-01 21:45:06 -0600

    Adding data
    Added a couple of json object arrays to represent our decks.

---
+++ data.js
@@ -0,0 +1,93 @@
+//The cards just have name, cost, and description. They are just for user convenience,
+//and won't be used by the sort program.
+
+var shortDeck = [
+	{
+		"name":"Cellar",
+		"cost":"2",
+		"description":"+1 Action. Discard any number of cards. +1 Card per card discarded."
+	},
+	{
+		"name":"Smithy",
+		"cost":"4",
+		"description":"+3 Cards."
+	},
+	{
+		"name":"Moat",
+		"cost":"2",
+		"description":"+2 Cards. When another player plays an Attack card, you may reveal this from your hand. If you do, you are unaffected by that Attack."
+	},
+	{
+		"name":"Chancellor",
+		"cost":"3",
+		"description":"+$2. You may immediately put your deck into your discard pile."
+	}
+];
+
+var longDeck = [
+	{
+		"name":"Cellar",
+		"cost":"2",
+		"description":"+1 Action. Discard any number of cards. +1 Card per card discarded."
+	},
+	{
+		"name":"Chapel",
+		"cost":"2",
+		"description":"Trash up to 4 cards from your hand."
+	},
+	{
+		"name":"Moat",
+		"cost":"2",
+		"description":"+2 Cards. When another player plays an Attack card, you may reveal this from your hand. If you do, you are unaffected by that Attack."
+	},
+	{
+		"name":"Chancellor",
+		"cost":"3",
+		"description":"+$2. You may immediately put your deck into your discard pile."
+	},
+	{
+		"name":"Chapel",
+		"cost":"2",
+		"description":"Trash up to 4 cards from your hand."
+	},
+	{
+		"name":"Village",
+		"cost":"3",
+		"description":"+1 Card; +2 Actions."
+	},
+	{
+		"name":"Woodcutter",
+		"cost":"3",
+		"description":"+1 Buy; +$2."
+	},
+	{
+		"name":"Workshop",
+		"cost":"3",
+		"description":"Gain a card costing up to $4."
+	},
+	{
+		"name":"Bureaucrat",
+		"cost":"4",
+		"description":"Gain a silver card; put it on top of your deck. Each other player reveals a Victory card from his hand and puts it on his deck (or reveals a hand with no Victory cards)."
+	},
+	{
+		"name":"Feast",
+		"cost":"4",
+		"description":"Trash this card. Gain a card costing up to $5."
+	},
+	{
+		"name":"Chapel",
+		"cost":"2",
+		"description":"Trash up to 4 cards from your hand."
+	},
+	{
+		"name":"Gardens",
+		"cost":"4",
+		"description":"Worth 1 Victory for every 10 cards in your deck (rounded down)."
+	},
+	{
+		"name":"Militia",
+		"cost":"4",
+		"description":"+$2. Each other player discards down to 3 cards in his hand."
+	}
+];
